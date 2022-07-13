f1a7d350a84045edb70b53d2127a927920a941df
Bruno Kappi de Matos <brunokappidematos2@gmail.com>
2022-07-13 10:09:48 -0300

    Comit

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
+module.exports = (env) => {
+  console.log("env",env)
+  return {
+    entry: './src/index.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }, {
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({  // Also generate a test.html
+        filename: 'index.html',
+        template: 'public/index.html'
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({  // Also generate a test.html
-      filename: 'index.html',
-      template:  'public/index.html'
-    })
-  ]
+  }
 }
